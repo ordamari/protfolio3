d737524f36cae556599327b921688f2ca17aab1c
ordamari <ordamari971@gmail.com>
2020-11-02 20:16:14 +0200

    Updates

---
+++ precache-manifest.f525ee03c238a96827504ca2dae6e495.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ec5906a26f6bce9267e6397319cab0",
+    "url": "/protfolio/index.html"
+  },
+  {
+    "revision": "637ff219529422b3b445",
+    "url": "/protfolio/static/css/main.b2d56d5b.chunk.css"
+  },
+  {
+    "revision": "d0d7134a928d242cd454",
+    "url": "/protfolio/static/js/2.04218b0c.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/protfolio/static/js/2.04218b0c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "637ff219529422b3b445",
+    "url": "/protfolio/static/js/main.b1ad9f9e.chunk.js"
+  },
+  {
+    "revision": "cf2d05ca2c03c8d34c3d",
+    "url": "/protfolio/static/js/runtime-main.99ba69e7.js"
+  },
+  {
+    "revision": "b20ff458bc0dc48c039c68a3dc96173c",
+    "url": "/protfolio/static/media/Caveat-Regular.b20ff458.ttf"
+  },
+  {
+    "revision": "9406d0ab606cf8cb91c41b65556bd836",
+    "url": "/protfolio/static/media/Lobster-Regular.9406d0ab.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/protfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d699480dd15f45b42295975434be947a",
+    "url": "/protfolio/static/media/angular.d699480d.png"
+  },
+  {
+    "revision": "c26a73b45b9a801864f13edeb0aa2cfb",
+    "url": "/protfolio/static/media/css.c26a73b4.png"
+  },
+  {
+    "revision": "52427607a6c373c7c8dbcf9dd305bd55",
+    "url": "/protfolio/static/media/cv.52427607.docx"
+  },
+  {
+    "revision": "cbba441819dedfd0d32e7ba009faee5e",
+    "url": "/protfolio/static/media/firebase.cbba4418.png"
+  },
+  {
+    "revision": "d991e2f9a54a1c233f71769a4e9309cc",
+    "url": "/protfolio/static/media/github.d991e2f9.png"
+  },
+  {
+    "revision": "664a8c00ef526bf33edf55808601d461",
+    "url": "/protfolio/static/media/html.664a8c00.png"
+  },
+  {
+    "revision": "86e8b2719e27754a947f14dedf93b0f7",
+    "url": "/protfolio/static/media/jquery.86e8b271.png"
+  },
+  {
+    "revision": "db7933e6a7f7acaf99a33f39b286974b",
+    "url": "/protfolio/static/media/js.db7933e6.png"
+  },
+  {
+    "revision": "f2d76ecbfe97ebb034cbe393dd36d551",
+    "url": "/protfolio/static/media/mdb.f2d76ecb.png"
+  },
+  {
+    "revision": "270f55fffea36219b66a63fbe03ff497",
+    "url": "/protfolio/static/media/nodejs.270f55ff.png"
+  },
+  {
+    "revision": "448ca321506e29744f5c46dc31bd05af",
+    "url": "/protfolio/static/media/react.448ca321.png"
+  },
+  {
+    "revision": "eac0fdba26c36d995b451da52e901668",
+    "url": "/protfolio/static/media/sass.eac0fdba.png"
+  },
+  {
+    "revision": "7097e5a0600ab47df3754e923a42f02f",
+    "url": "/protfolio/static/media/vue.7097e5a0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/protfolio/precache-manifest.c953391c070bcb431624acd4b7676976.js"
+  "/protfolio/precache-manifest.f525ee03c238a96827504ca2dae6e495.js"
 );
 
 self.addEventListener('message', (event) => {
