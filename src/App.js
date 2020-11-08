0d7451b2f781f472f0ede7fdaf4d91fc4b12f0f2
ordamari <ordamari971@gmail.com>
2020-11-08 21:01:24 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import { Link } from 'react-router-dom'
-import { Footer } from './cmps/Footer';
-import { Navbar } from './cmps/Navbar';
-import { ProjectDetails } from './cmps/ProjectDetails';
 
 import { Home } from './pages/Home';
 
@@ -12,12 +8,9 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
         <Switch>
-          <Route component={ProjectDetails} path='/:id' />
           <Route component={Home} path='/' />
         </Switch>
-        <Footer />
       </Router>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,78 +1,12 @@
 import React from 'react';
-import { useEffect, useRef, useState } from 'react';
-import { About } from '../cmps/About';
-import { Protfolio } from '../cmps/Protfolio';
 import { Specialities } from '../cmps/Specialities';
-import projectService from '../services/projectService';
-import positionService from '../services/positionService';
-import eventBus from '../services/event-bus-service'
 
 
 
 export function Home(props) {
-
-    const [txt, setTxt] = useState('');
-    const [isOpen, setIsOpen] = useState(false);
-    var str = 'FullStack Web Developer';
-    var interval = '';
-    var unsubscribe  = null;
-
-    const projects = projectService.query();
-
-    function scrollToTareget(target) {
-        let pos = 0
-        if (target) pos = positionService.query()[target] - 72
-        window.scrollTo(0, pos)
-    }
-
-    useEffect(() => {
-        interval = setInterval(() => {
-            if (txt !== str) setTxt((prevText => prevText + str.charAt(prevText.length)))
-        }, 200);
-
-        unsubscribe = eventBus.on('toggle-is-open', (data) => {
-            setIsOpen(!data)
-        })
-
-        return ()=>{
-            clearInterval(interval)
-            unsubscribe=null
-        }
-
-
-
-    }, [])
-
-    useEffect(() => {
-       if(txt===str) setTimeout(() => {
-           setTxt('');
-       }, 1000);
-    }, [txt]);
-
-
-    function closeWhenOpen(){
-        return isOpen? 'hide':''
-    }
-
-
-
-
-
-
     return (
-        <section className='home'>
-            <div className='main-img'>
-                <div className={`main-info ${closeWhenOpen()}`}>
-                    <h2>Hi, I'm Or Damari.</h2>
-                    <h1>{txt}<span>|</span></h1>
-                    {/* <h1>Full Stack Web Developer</h1> */}
-                    <button  onClick={() => scrollToTareget('specialties')}>Show more</button>
-                </div>
-            </div>
+        <section>
             <Specialities />
-            <Protfolio projects={projects} />
-            <About />
-
         </section>
     )
 
